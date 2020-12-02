7e95db9b6231aa8f47e08569beaf6e79fafaaa78
Lizhanyun <sophiehal@163.com>
2020-12-02 21:58:43 +0800

    初始创建

---
+++ api/src/test/java/com/sophie/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophie.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
