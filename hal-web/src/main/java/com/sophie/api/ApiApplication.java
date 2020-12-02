8355d11961fb7e86bed17f907070cccbb642696f
Lizhanyun <sophiehal@163.com>
2020-12-03 00:13:38 +0800

    第二次提交

--- hal-web/src/main/java/com/sophie/api/ApiApplication.java
+++ hal-web/src/main/java/com/sophie/api/ApiApplication.java
@@ -1,8 +1,10 @@
 package com.sophie.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("hal-dao")
 @SpringBootApplication
 public class ApiApplication {
 
